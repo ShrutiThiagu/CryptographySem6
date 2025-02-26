e7dd2d75082236f24c3036a6264356d6cd72e687
ShrutiThiagu <137478885+ShrutiThiagu@users.noreply.github.com>
2025-02-26 11:52:19 +0530

    Update 13_railfence_cipher.c

--- 13_railfence_cipher.c
+++ 13_railfence_cipher.c
@@ -4,20 +4,24 @@
 void rail_fence_encrypt(char *PT, int rails, char *CT) {
     int len = strlen(PT);
     char rail[rails][len];
-    memset(rail, '\n', sizeof(rail));
+    memset(rail, ' ', sizeof(rail));
     int row = 0, direction = 1;
     for (int i = 0; i < len; i++) {
         rail[row][i] = PT[i];
-        if (row == 0)
-            direction = 1;
-        else if (row == rails - 1)
-            direction = -1;
+        if (row == 0) direction = 1;
+        else if (row == rails - 1) direction = -1;
         row += direction;
+    }
+    for (int i = 0; i < rails; i++) {
+        for (int j = 0; j < len; j++) {
+            printf("%c ", rail[i][j]);
+        }
+        printf("\n");
     }
     int index = 0;
     for (int i = 0; i < rails; i++) {
         for (int j = 0; j < len; j++) {
-            if (rail[i][j] != '\n') {
+            if (rail[i][j] != ' ') {
                 CT[index++] = rail[i][j];
             }
         }
@@ -28,16 +32,16 @@ void rail_fence_encrypt(char *PT, int rails, char *CT) {
 void rail_fence_decrypt(char *CT, int rails, char *PT) {
     int len = strlen(CT);
     char rail[rails][len];
-    memset(rail, '\n', sizeof(rail));
+    memset(rail, ' ', sizeof(rail));
+
     int row = 0, direction = 1;
     for (int i = 0; i < len; i++) {
         rail[row][i] = '*';
-        if (row == 0)
-            direction = 1;
-        else if (row == rails - 1)
-            direction = -1;
+        if (row == 0) direction = 1;
+        else if (row == rails - 1) direction = -1;
         row += direction;
     }
+
     int index = 0;
     for (int i = 0; i < rails; i++) {
         for (int j = 0; j < len; j++) {
@@ -46,13 +50,12 @@ void rail_fence_decrypt(char *CT, int rails, char *PT) {
             }
         }
     }
+
     row = 0, direction = 1;
     for (int i = 0; i < len; i++) {
         PT[i] = rail[row][i];
-        if (row == 0)
-            direction = 1;
-        else if (row == rails - 1)
-            direction = -1;
+        if (row == 0) direction = 1;
+        else if (row == rails - 1) direction = -1;
         row += direction;
     }
     PT[len] = '\0';
@@ -61,15 +64,16 @@ void rail_fence_decrypt(char *CT, int rails, char *PT) {
 int main() {
     char text[100];
     printf("Enter text: ");
-    scanf("%s",text);
+    scanf("%s", text);
     int rails;
     printf("Enter the number of rails: ");
-    scanf("%d",&rails);
+    scanf("%d", &rails);
+
     char encrypted[100], decrypted[100];
     rail_fence_encrypt(text, rails, encrypted);
-    printf("Encrypted: %s\n", encrypted);
+    printf("\nEncrypted Text: %s\n", encrypted);
     rail_fence_decrypt(encrypted, rails, decrypted);
-    printf("Decrypted: %s\n", decrypted);
+    printf("\nDecrypted Text: %s\n", decrypted);
+    
     return 0;
 }
-
